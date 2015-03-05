aaf918e480b164e39150ffeb486c28daf1035b1a
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-03-04 22:23:36 -0500

    Implementing caching.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var http = require('http');
 var request = require('request');
 var url = require('url');
+var redis = require('redis');
 
 var apiKey = Secret['api-key'];
+
+// create redis client
+var client = redis.createClient(6379, '127.0.0.1');
 
 var server = http.createServer(function(req, res){
     var link = url.format({
@@ -15,10 +19,31 @@ var server = http.createServer(function(req, res){
         pathname: req.url
     });
 
-    request.get(link)
-        .pipe(res);
+    client.get(req.url, function(err, reply){
+        if (reply) {
+            res.end(reply);
+            return;
+        }
+
+        request.get({url: link, json:false}, function(e, r, payload){
+            client.set(req.url, payload, function(){
+                client.expire(req.url, 24*60*60, function(){
+                    res.end(payload);
+                });
+            });
+        });
+    });
+
+});
+
+client.on('error', function(err){
+    console.log(err);
 });
 
 server.listen(9000, function(){
     console.log('Starting proxy on port 9000!');
+});
+
+server.on('close', function(){
+    client.quit();
 });
