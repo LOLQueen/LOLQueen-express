07d5fb64b95072fcd8e0e94ce1a33d6e58ecb6c7
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-10-17 21:17:10 -0400

    Adding caching to the fetchChampion method.

--- services/RIOTApi/index.js
+++ services/RIOTApi/index.js
@@ -5,6 +5,8 @@ import {
   transformSummoner,
 } from './transforms';
 
+import {prop, values} from 'ramda';
+
 export async function fetchSummoners({names, region}) {
   expect(names).to.be.an('array');
   return fetchFromRiot({
@@ -13,10 +15,9 @@ export async function fetchSummoners({names, region}) {
 }
 
 export async function fetchSummoner({region, id}) {
-  const summoners = await fetchFromRiot({
+  return id && (await fetchFromRiot({
     region, url: `v1.4/summoner/${id}`
-  });
-  return id && transformSummoner(summoners[id]);
+  }))[id];
 }
 
 export async function fetchGames({region, summonerId}) {
@@ -25,11 +26,23 @@ export async function fetchGames({region, summonerId}) {
   });
 }
 
-export async function fetchChampion({region, id}) {
-  return id && transformChampion(await fetchStaticFromRiot({
-    region, url: `v1.2/champion/${id}`
-  }));
+async function fetchChampions({region}) {
+  const response = await fetchStaticFromRiot({
+    region, url: `v1.2/champion`
+  });
+
+  return values(response.data).reduce((map, champ) => (
+    map[champ.id] = transformChampion(champ), map
+  ), {});
 }
+
+export const fetchChampion = () => {
+  const store = {};
+  return async function fetch({region, id}) {
+    store[region] = store[region] || await fetchChampions({region});
+    return id && store[region][id];
+  };
+}();
 
 export async function fetchSpell({region, id}) {
   return id && fetchStaticFromRiot({
