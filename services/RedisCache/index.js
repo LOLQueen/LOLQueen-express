03ce2068d899ab4dd3b685ebcefbb6f27425dc28
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-10-18 16:34:02 -0400

    Making code in RedisCache service more verbose.

--- services/RedisCache/index.js
+++ services/RedisCache/index.js
@@ -1,6 +1,10 @@
 import ExpressRedisCache from 'express-redis-cache';
 
-const RedisCache = ExpressRedisCache();
+const TEN_MINUTES = 60 * 10;
+
+const RedisCache = ExpressRedisCache({
+  expire: TEN_MINUTES,
+});
 
 export default RedisCache;
 export const cache = ::RedisCache.route;
