52d8a14892fb9c0083a7faf1b8504224d8a9943b
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-10-31 18:50:42 -0400

    Core fetch functions now retry requests if we get a 429 from RIOT.

--- services/RIOTApi/core.js
+++ services/RIOTApi/core.js
@@ -1,20 +1,35 @@
 import fetch from 'isomorphic-fetch';
 import { stringify } from 'querystring';
-import { prop, composeP, invoker } from 'ramda';
+import { prop, composeP, invoker, merge } from 'ramda';
 import { API_KEY } from 'secrets';
+import bluebird from 'bluebird';
 
 const RIOT_API = `https://na.api.pvp.net/api/lol`;
 const RIOT_STATIC_API = `https://global.api.pvp.net/api/lol/static-data`;
 
 function createFetchFn(apiUrl) {
-  return function fetchFn({ region, url }) {
-    const tokenQS = stringify({ 'api_key': API_KEY });
-    const location = `${apiUrl}/${region}/${url}?${tokenQS}`;
+  return function fetchFn({ region, url, query = {} }) {
+    const qs = stringify(merge(query, { 'api_key': API_KEY }));
+    const location = `${apiUrl}/${region}/${url}?${qs}`;
 
     return fetch(location)
       .then(checkStatus)
+      .catch(retry)
       .then(invoker(0, 'json'));
   };
+}
+
+function retry(error) {
+  const { status, url, headers } = error.response;
+  switch (status) {
+    case 429:
+      const seconds = Number(headers.get('retry-after')[0]);
+      return bluebird
+        .delay(seconds * 1000)
+        .then(() => fetch(url));
+    default:
+      throw error;
+  }
 }
 
 export const fetchFromRiot = createFetchFn(RIOT_API);
